1d68cc991e016752e743b2852d90e6dd9776aac1
Denbeigh Stevens <denbeigh.stevens@discordapp.com>
2022-06-05 09:19:31 -0700

    dev: don't install go on work devices

--- dev.nix
+++ dev.nix
@@ -5,9 +5,9 @@ let
   inherit (pkgs.devPackages) python rust go node nix;
 
   python-packages = if !work then [ python.python310 ] else [ ];
-
+  go-packages = if !work then go.all else [ go.gopls ];
   extra-node-packages = if !work then [ node.yarn ] else [ ];
   node-packages = node.allNode18 ++ extra-node-packages;
   nix-packages = [ nix.rnix-lsp ];
 in
-rust.all ++ go.all ++ node-packages ++ python-packages ++ nix-packages
+rust.all ++ go-packages ++ node-packages ++ python-packages ++ nix-packages
