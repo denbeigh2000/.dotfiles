63fdb5a93c18325a6a91b3d3e926b766a045676c
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2022-09-11 02:21:04 -0700

    re-pin to 22.05?

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Denbeigh's Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
