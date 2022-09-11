0492520435d2363d7e0c71d024ded79e8032a852
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2022-09-11 02:37:16 -0700

    maybe fix home-manager config in nixos?

--- nixos/modules/denbeigh.nix
+++ nixos/modules/denbeigh.nix
@@ -1,7 +1,6 @@
 { lib
 , pkgs
 , home-manager
-, homeConfigurations
 , hostname
 , ...
 }:
@@ -20,16 +19,52 @@ in
   };
 
   config = {
-    home-manager = {
-      useGlobalPkgs = true;
-      useUserPackages = true;
-      users.denbeigh = homeConfigurations.${hmConfig};
-    };
-
     users.users.denbeigh = {
       isNormalUser = true;
       extraGroups = [ "wheel" ];
       shell = pkgs.zsh;
+    };
+
+    home-manager = {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      # TODO: This doesn't seem to be the right thing to do:
+      # https://nix-community.github.io/home-manager/index.html#sec-flakes-nixos-module
+      # Need to find an example of something that's done this
+      users.denbeigh = {
+        imports = [
+          # ../../modules/dev.nix
+          ../../modules/git.nix
+          # ../../modules/linux.nix
+          # ../../modules/zsh
+        ];
+
+        # TODO: Don't copy-paste
+        home = {
+          username = "denbeigh";
+          homeDirectory = "/home/denbeigh";
+          packages = with pkgs; [ ripgrep ];
+        };
+
+        programs = {
+          # Let Home Manager install and manage itself.
+          home-manager.enable = true;
+
+          aria2.enable = true;
+          jq.enable = true;
+
+          # keychain = {
+          #   enable = host.keys != null;
+          #   inherit (host) keys;
+          # };
+
+          gh.enable = true;
+
+          fzf.enable = true;
+        };
+
+        home.stateVersion = "22.05";
+      };
     };
   };
 }
