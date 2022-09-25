22e7f01bfc6a0e8c7e41a789297db2062b9ccab8
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2022-09-24 21:06:41 -0700

    remove root req from editing tool

--- tools/agenix-edit.nix
+++ tools/agenix-edit.nix
@@ -3,12 +3,6 @@
 pkgs.writeShellScriptBin "agenix-edit" ''
   set -euo pipefail
 
-  if [[ "$UID" -ne 0 ]]
-  then
-    echo "This script must be run as root" >&2
-    exit 1
-  fi
-
   if [[ -e /var/lib/denbeigh/host_key ]]
   then
     ${pkgs.agenix}/bin/agenix -i /var/lib/denbeigh/host_key -e "$1"
