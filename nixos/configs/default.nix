2c670bf0d1b9b1024eac23d34161f24ac66e304a
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2022-12-10 15:13:44 -0800

    nixos: remove hard-coded systems map

--- nixos/configs/default.nix
+++ nixos/configs/default.nix
@@ -4,12 +4,24 @@ let
   inherit (builtins) mapAttrs;
   inherit (nixpkgs.lib) nixosSystem;
 
-  configs = {
-    bruce = ./bruce.nix;
-    coder-ec2-x86 = ./coder-ec2-x86.nix;
-    coder-ec2-aarch64 = ./coder-ec2-aarch64.nix;
-    coder-ec2-aarch64-plain = ./coder-ec2-aarch64-plain.nix;
-  };
+  configs =
+    let
+      inherit (builtins) path readDir stringLength substring;
+      inherit (nixpkgs.lib.attrsets) filterAttrs mapAttrs';
+      inherit (nixpkgs.lib.strings) hasSuffix;
+
+      pwd = path { path = ./.; name = "config-pwd"; };
+      isModule = (n: v:
+        n != "default.nix" && v == "regular" && (hasSuffix ".nix" n));
+
+      modules = filterAttrs isModule (readDir pwd);
+      toConfig = filename: _:
+        let
+          name = substring 0 ((stringLength filename) - 4) filename;
+        in
+        { inherit name; value = ./${filename}; };
+    in
+    mapAttrs' toConfig modules;
 
   defaults = { pkgs, ... }:
     let
