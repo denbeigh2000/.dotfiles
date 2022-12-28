24704bcfa01e8f0301e5ed33cfbf9c87b1737466
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2022-12-27 17:26:03 -0800

    home-manager: fix hosts without explicit autorandr config

--- home-manager/modules/autorandr/default.nix
+++ home-manager/modules/autorandr/default.nix
@@ -1,15 +1,24 @@
 { config, pkgs, lib, ... }:
 
 let
-  inherit (builtins) pathExists;
-  inherit (lib) mkIf;
+  inherit (builtins) path readDir stringLength substring;
+  inherit (lib) mapAttrs' mkIf;
   inherit (config.denbeigh) hostname;
 
-  configPath = ./configs/${hostname}.nix;
+  fileToConfig = filename: _:
+    let
+      name = substring 0 ((stringLength filename) - 4) filename;
+    in
+    { inherit name; value = import ./${filename}; };
+
+  configFiles = readDir ./configs;
+  configs = mapAttrs' fileToConfig configFiles;
+  exists = configs ? hostname;
+  config = if exists then configs.${hostname} else {};
 in
 {
-  # TODO: Support this in configuration directly
-  programs.autorandr = mkIf (pathExists configPath) ({
+  # TODO: Define these in host-level configuration directly
+  programs.autorandr = mkIf exists ({
     enable = true;
-  } // (import configPath));
+  } // config);
 }
