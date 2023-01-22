446f5107412f033afda23aebb5a7c755f057e53b
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-01-22 15:38:40 -0800

    nixos graphical: remove graphical stuff from non-graphical machines

--- nixos/modules/graphical.nix
+++ nixos/modules/graphical.nix
@@ -1,61 +1,67 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   inherit (pkgs) alacritty writeShellScriptBin;
   inherit (pkgs.denbeigh.fonts) sf-mono;
+  inherit (lib) mkIf;
   inherit (config.denbeigh.user) username;
+  inherit (config.denbeigh.machine) graphical;
+
 in
 {
-  nixpkgs.config.allowUnfree = true;
-
-  users = {
-    mutableUsers = true;
+  config = mkIf graphical {
+    nixpkgs.config.allowUnfree = true;
 
     users = {
-      root.initialHashedPassword = "$y$j9T$TK9zFbNlrHL9vroc.uBZC1$FRhAYk/Y4LY480eesYdxDIDOvttgzcmmaB0vF3ge.1C";
-      ${username} = {
-        initialHashedPassword = "$y$j9T$TK9zFbNlrHL9vroc.uBZC1$FRhAYk/Y4LY480eesYdxDIDOvttgzcmmaB0vF3ge.1C";
-        extraGroups = [ "audio" ];
+      mutableUsers = true;
+
+      users = {
+        root.initialHashedPassword = "$y$j9T$TK9zFbNlrHL9vroc.uBZC1$FRhAYk/Y4LY480eesYdxDIDOvttgzcmmaB0vF3ge.1C";
+        ${username} = {
+          initialHashedPassword = "$y$j9T$TK9zFbNlrHL9vroc.uBZC1$FRhAYk/Y4LY480eesYdxDIDOvttgzcmmaB0vF3ge.1C";
+          extraGroups = [ "audio" ];
+        };
       };
     };
-  };
 
-  environment.systemPackages = with pkgs; [
-    alacritty
-    discord-canary
-    firefox
-    nix
-    openssh
-    pavucontrol
-    scrot
-    spotify
-    vim
-  ];
+    environment.systemPackages = with pkgs; [
+      alacritty
+      discord-canary
+      firefox
+      nix
+      openssh
+      pavucontrol
+      scrot
+      spotify
+      vim
+    ];
 
-  console = {
-    enable = true;
-  };
-
-  nixpkgs.config.pulseaudio = true;
-  hardware.pulseaudio = {
-    enable = true;
-    extraConfig = "load-module module-combine-sink";
-    package = pkgs.pulseaudioFull;
-    support32Bit = true;
-  };
-
-
-  # NOTE: We explicitly don't use provide a config here, and defer to the
-  # config populated by home-manager (see home-manager/modules/i3)
-  services.xserver = {
-    enable = true;
-    displayManager.sddm.enable = true;
-    windowManager.i3 = {
+    console = {
       enable = true;
-      extraPackages = with pkgs; [ dmenu i3lock ];
-      extraSessionCommands = ''
-        export TERMINAL="${alacritty}/bin/alacritty"
-      '';
     };
+
+    nixpkgs.config.pulseaudio = true;
+    hardware.pulseaudio = {
+      enable = true;
+      extraConfig = "load-module module-combine-sink";
+      package = pkgs.pulseaudioFull;
+      support32Bit = true;
+    };
+
+
+    # NOTE: We explicitly don't use provide a config here, and defer to the
+    # config populated by home-manager (see home-manager/modules/i3)
+    services.xserver = {
+      enable = true;
+      displayManager.sddm.enable = true;
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [ dmenu i3lock ];
+        extraSessionCommands = ''
+          export TERMINAL="${alacritty}/bin/alacritty"
+        '';
+      };
+    };
+
   };
 }
