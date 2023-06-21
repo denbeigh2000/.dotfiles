7d1d7c0d5d50463f3e1bf63f2b1026949b883483
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-06-21 21:20:11 +1000

    nix-darwin lucifer: use tailscale

--- nix-darwin/configs/lucifer.nix
+++ nix-darwin/configs/lucifer.nix
@@ -2,7 +2,7 @@
   system = "aarch64-darwin";
   modules = [
     {
-      imports = [ ../modules/standard.nix ];
+      imports = [ ../modules/standard.nix ../modules/tailscale.nix ];
       config.denbeigh = {
         machine = {
           hostname = "lucifer";
