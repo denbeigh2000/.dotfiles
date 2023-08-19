9501b285d0fcf0cd9887559483c5b387ce083ff2
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-08-19 01:04:37 -0700

    modules upload-daemon: deduplicate cross-os module

---
+++ modules/common/upload-daemon.nix
@@ -0,0 +1,72 @@
+{ config, lib, nix-upload-daemon, ... }:
+
+let
+  inherit (config.denbeigh) machine;
+  cfg = config.denbeigh.nix-upload-daemon;
+in
+{
+  options = {
+    denbeigh.nix-upload-daemon =
+      let
+        inherit (lib) mkOption types;
+      in
+      {
+        binding = mkOption {
+          type = types.str;
+          default = "sock:///tmp/nix-upload-daemon.sock";
+        };
+
+        target = mkOption {
+          type = types.str;
+          default = "ssh-ng://nix-copy-receiver@bruce.denbeigh.cloud?ssh-key=${config.age.secrets.remoteBuildUploadKey.path}";
+          description = "Store to upload to";
+        };
+
+        workers = mkOption {
+          type = types.int;
+          default = 2;
+          description = "Number of workers to run";
+        };
+
+        username = mkOption {
+          type = types.str;
+          default = "nix-upload-daemon";
+          description = "User to run uploader daemon as";
+        };
+      };
+  };
+
+  config = {
+    assertions = [
+      {
+        assertion = !(machine.work && cfg.enable);
+        message = "Do **NOT** upload work packages to personal nix store";
+      }
+    ];
+  } // mkIf cfg.enable ({
+    nixpkgs.overlays = [ nix-upload-daemon.overlays.default ];
+
+    age.secrets.remoteBuildSignKey = {
+      file = ../../secrets/remoteBuildSignKey.age;
+      owner = cfg.username;
+      mode = "600";
+    };
+
+    age.secrets.remoteBuildUploadKey = {
+      file = ../../secrets/remoteBuildUploadKey.age;
+      owner = cfg.username;
+      mode = "600";
+    };
+
+    services.nix-upload-daemon = {
+      inherit (cfg) enable username workers target binding;
+
+      post-build-hook = {
+        enable = true;
+        secretKey = config.age.secrets.remoteBuildSignKey.path;
+      };
+    };
+
+    nix.settings.trusted-users = [ config.denbeigh.user.username ];
+  });
+}
--- modules/darwin/upload-daemon.nix
+++ modules/darwin/upload-daemon.nix
@@ -7,84 +7,25 @@ in
   imports = [
     agenix.darwinModules.default
     nix-upload-daemon.darwinModules.default
+    ../common/upload-daemon.nix
   ];
 
   options = {
-    denbeigh.nix-upload-daemon =
-      let
-        inherit (lib) mkOption types;
-      in
-      {
-        enable = mkOption {
-          type = types.bool;
-          default = !machine.work;
-        };
-
-        binding = mkOption {
-          type = types.str;
-          default = "sock:///tmp/nix-upload-daemon.sock";
-        };
-
-        target = mkOption {
-          type = types.str;
-          default = "ssh-ng://nix-copy-receiver@bruce.denbeigh.cloud?ssh-key=${config.age.secrets.remoteBuildUploadKey.path}";
-          description = "Store to upload to";
-        };
-
-        workers = mkOption {
-          type = types.int;
-          default = 2;
-          description = "Number of workers to run";
-        };
-
-        username = mkOption {
-          type = types.str;
-          default = "nix-upload-daemon";
-          description = "User to run uploader daemon as";
-        };
-      };
+    denbeigh.nix-upload-daemon.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = !machine.work;
+    };
   };
   config =
     let
       inherit (lib) mkIf;
       cfg = config.denbeigh.nix-upload-daemon;
     in
-    {
-      assertions = [
-        {
-          assertion = !(machine.work && cfg.enable);
-          message = "Do **NOT** upload work packages to personal nix store";
-        }
-      ];
-    } // (mkIf cfg.enable {
-      nixpkgs.overlays = [ nix-upload-daemon.overlays.default ];
-
+    mkIf cfg.enable {
       age.identityPaths =
         let
           inherit (config.denbeigh.user) username keys;
         in
         builtins.map (key: "/Users/${username}/.ssh/${key}") keys;
-      age.secrets.remoteBuildSignKey = {
-        file = ../../secrets/remoteBuildSignKey.age;
-        owner = config.denbeigh.user.username;
-        mode = "600";
-      };
-
-      age.secrets.remoteBuildUploadKey = {
-        file = ../../secrets/remoteBuildUploadKey.age;
-        owner = cfg.username;
-        mode = "600";
-      };
-
-      nix.settings.trusted-users = [ config.denbeigh.user.username ];
-
-      services.nix-upload-daemon = {
-        inherit (cfg) enable username workers target binding;
-
-        post-build-hook = {
-          enable = true;
-          secretKey = config.age.secrets.remoteBuildSignKey.path;
-        };
-      };
-    });
+    };
 }
