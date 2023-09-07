c7b1ba333cc9380f6ce7fe63fc4a3112c8c027e9
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-09-06 22:09:13 -0700

    home-manager: remove some use of overlays (for now)

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -1,9 +1,15 @@
 { self, pkgs, ... }:
 
+let
+  inherit (pkgs.stdenvNoCC.hostPlatform) system;
+  # TODO: Using overlays in standalone HM configs?
+  inherit (self.inputs.fonts.packages.${system}) sf-mono sf-pro;
+in
 {
-  home.packages = with pkgs; [
-    denbeigh.fonts.sf-mono
-    denbeigh.fonts.sf-pro
-    powerline-fonts
+  nixpkgs.overlays = [ self.inputs.fonts.overlays.default ];
+  home.packages = [
+    sf-mono
+    sf-pro
+    pkgs.powerline-fonts
   ];
 }
