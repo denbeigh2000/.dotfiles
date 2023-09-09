b0a87bb86c8fcf1be78dda28710c0a7edf55f82f
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-09-09 11:32:06 -0700

    modules aws: install awscli by default

--- modules/nixos/cloud/aws/default.nix
+++ modules/nixos/cloud/aws/default.nix
@@ -1,7 +1,10 @@
-{ modulesPath, lib, ... }:
+{ modulesPath, lib, pkgs, ... }:
 
 {
   imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
   ec2.hvm = true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  # Install AWS CLI by default on AWS machines
+  environment.systemPackages = [ pkgs.awscli2 ];
 }
