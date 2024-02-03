2aff18a3f43b76c392e6a481abe80ab898ba3eed
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2024-02-03 11:39:59 -0800

    nixpkgs: use more modern version of cfdyndns

--- unstable-overlay.nix
+++ unstable-overlay.nix
@@ -1,5 +1,5 @@
 { nixpkgs-unstable
-# , goi3bar-src
+  # , goi3bar-src
 , noisetorch-src
 , ...
 }:
@@ -14,8 +14,8 @@ in
   # These are very far apart, and have large feature gaps
   inherit (pkgs-unstable) radarr sonarr prowlarr;
 
-  # This isn't backported as frequently as I'd like
-  inherit (pkgs-unstable) tailscale;
+  # These aren't backported as frequently as I'd like
+  inherit (pkgs-unstable) tailscale cfdyndns;
 
   # Various 3rdparty packages
   # goi3bar = final.callPackage ./3rdparty/goi3bar { inherit goi3bar-src; };
