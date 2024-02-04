8de98bb91ed2ffcb969bb73af2730e6c659ffe77
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2024-02-03 16:29:00 -0800

    nixos router: fix issues with kea

--- modules/nixos/router/dhcp.nix
+++ modules/nixos/router/dhcp.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 let
   inherit (config.denbeigh.services.router) interfaces;
@@ -7,19 +7,19 @@ in
   config = {
     # NOTE: This is usually created implicitly by dhcpd's systemd job, but we
     # need it to exist by the time this is being run.
+    # https://github.com/NixOS/nixpkgs/blob/25e3d4c0d3591c99929b1ec07883177f6ea70c9d/nixos/modules/services/networking/kea.nix#L255
     users = {
-      users.dhcpd = {
+      users.kea = {
         isSystemUser = true;
-        description = "dhcpd service user";
-        group = "${config.users.groups.dhcpd.name}";
+        description = "kea service user";
+        group = "${config.users.groups.kea.name}";
       };
-      groups.dhcpd = { };
+      groups.kea = { };
     };
 
     age.secrets.dhcp-dns-update-key = {
       file = ../../../secrets/dhcpDnsAuthKeyKea.age;
-      # https://github.com/NixOS/nixpkgs/blob/25e3d4c0d3591c99929b1ec07883177f6ea70c9d/nixos/modules/services/networking/kea.nix#L255
-      owner = "kea";
+      owner = config.users.users.kea.name;
     };
 
     networking = {
@@ -46,6 +46,29 @@ in
               name = "/var/lib/private/kea/dhcp4.leases";
             };
 
+            option-data = [
+              {
+                name = "domain-name-servers";
+                data = "10.69.1.1";
+              }
+              {
+                name = "broadcast-address";
+                data = "10.69.1.255";
+              }
+              {
+                name = "routers";
+                data = "10.69.1.1";
+              }
+              {
+                name = "domain-name";
+                data = "sfo.denbeigh.cloud";
+              }
+              {
+                name = "domain-search";
+                data = "sfo.denbeigh.cloud";
+              }
+            ];
+
             subnet4 = [
               {
                 subnet = "10.69.1.0/24";
@@ -61,32 +84,46 @@ in
 
         dhcp-ddns = {
           enable = true;
-          settings = {
-            forward-ddns = {
-              ddns-domains = [
+          # NOTE: can't reference a secret from a file without doing this
+          configFile =
+            let
+              configContent = ''
                 {
-                  name = "sfo.denbeigh.cloud.";
-                  key-name = "local";
-                  dns-servers = [{ "ip-address" = "127.0.0.1"; }];
+                  "DhcpDdns": {
+                    "forward-ddns": {
+                      "ddns-domains": [
+                        {
+                          "dns-servers": [
+                            {
+                              "ip-address": "127.0.0.1"
+                            }
+                          ],
+                          "key-name": "local",
+                          "name": "sfo.denbeigh.cloud."
+                        }
+                      ]
+                    },
+                    "reverse-ddns": {
+                      "ddns-domains": [
+                        {
+                          "dns-servers": [
+                            {
+                              "ip-address": "127.0.0.1"
+                            }
+                          ],
+                          "key-name": "local",
+                          "name": ".in-addr.arpa."
+                        }
+                      ]
+                    },
+                    "tsig-keys": [
+                      <?include "${config.age.secrets.dhcp-dns-update-key.path}"?>
+                    ]
+                  }
                 }
-              ];
-            };
-            reverse-ddns = {
-              ddns-domains = [
-                {
-                  name = ".in-addr.arpa.";
-                  key-name = "local";
-                  dns-servers = [{ ip-address = "127.0.0.1"; }];
-                }
-              ];
-            };
-            tsig-keys = {
-              name = "local";
-              algorithm = "HMAC-SHA256";
-              # TODO: test <?include > works from within a string
-              secret = "<?include \"${config.age.secrets.dhcp-dns-update-key.path}\">";
-            };
-          };
+              '';
+            in
+            pkgs.writeText "ddns.conf" configContent;
         };
       };
     };
